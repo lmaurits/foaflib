d9b796398f2b3b2d07a746a1584defd8915652b8
luke.maurits <luke.maurits@7a45f282-4ea1-11de-9ad6-1127a62b9fcd>
2009-06-13 13:13:31 +0000

    Added ability to derive feedformatter Feed objects from ActivityStreams.

--- trunk/foaflib/utils/activitystream.py
+++ trunk/foaflib/utils/activitystream.py
@@ -3,6 +3,12 @@ from foaflib.helpers.delicious import Delicious
 from foaflib.helpers.identica import Identica
 from foaflib.helpers.twitter_ import Twitter
 
+try:
+    from feedformatter import Feed
+    _CAN_DO_FEEDS_ = True
+except ImportError:
+    _CAN_DO_FEEDS_ = False
+
 _ALL_HELPERS_ = [Delicious]
 _ALL_HELPERS_.append(Identica)
 _ALL_HELPERS_.append(Twitter)
@@ -23,3 +29,20 @@ class ActivityStream(object):
         events.sort()
         events.reverse()
         return events[0:count]
+
+    def build_feed(self):
+        if not _CAN_DO_FEEDS_:
+            return None
+        feed = Feed()
+        feed.feed["title"] = self.person.name + "'s Activity Stream"
+        feed.feed["description"] = self.person.name + "'s Activity Stream"
+        feed.feed["link"] = self.person.uri
+        feed.feed["author"] = self.person.name
+        for event in self.get_latest_events():
+            item = {}
+            item["title"] = event.type + " - " + event.detail
+            item["pubDate"] = event.timestamp
+            item["link"] = event.link
+            item["guid"] = event.link
+            feed.items.append(item)
+        return feed
